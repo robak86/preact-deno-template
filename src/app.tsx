bdd2a465c0051aababa78a3dd75bdef5d6a14e11
theArianit <arianit.029@hotmail.com>
2022-09-14 23:00:28 +0200

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -4,7 +4,7 @@ export default function App() {
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <title>Ultra</title>
+        <title>Preact Deno</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body>
