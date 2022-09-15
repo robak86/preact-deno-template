ea6e4f6dc07a21fa9c43f504bb296412f9be93ee
theArianit <arianit.029@hotmail.com>
2022-09-15 11:43:40 +0200

    Update client.ts

--- client.ts
+++ client.ts
@@ -1,6 +1,6 @@
 // for client side hydration
 
-import { hydrateRoot } from "preact-dom/client";
+import { hydrateRoot } from "react-dom/client";
 import App from "./src/app.tsx";
 
 hydrateRoot(document, <App />);
